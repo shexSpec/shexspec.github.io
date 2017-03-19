90c4864d5fcd20ebca069771d41787d5bba190c0
Gregg Kellogg <gregg@greggkellogg.net>
2017-03-19 13:36:57 -0700

    Make namespace location consistent in namespace HTML file, and move all files to be named for ns/shex. ns/index will redirect to ns/shex.html from shex.io site.

--- ns/mk_vocab.rb
+++ ns/mk_vocab.rb
@@ -402,7 +402,7 @@ when :html    then options[:output].puts(vocab.to_html)
 when :shexc   then options[:output].puts(vocab.to_shexc)
 else
   [:jsonld, :ttl, :html, :shexc].each do |format|
-    fn = {jsonld: "context.jsonld", ttl: "shex.ttl", html: "index.html",shexc: "shex.shexc"}[format]
+    fn = {jsonld: "shex.jsonld", ttl: "shex.ttl", html: "shex.html", shexc: "shex.shexc"}[format]
     File.open(fn, "w") do |output|
       output.puts(vocab.send("to_#{format}".to_sym))
     end
